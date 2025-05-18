35db149955d561936271bb1e5353666f73751fc2
finemold2 <104006514+finemold2@users.noreply.github.com>
2025-05-19 00:06:06 +0900

    a

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.download-btn').forEach(function (btn) {
     btn.addEventListener('click', function (e) {
         e.preventDefault();
-        alert('앱 다운로드는 곧 제공될 예정입니다!');
+        alert('앱 다운로드는 곧 제공될 예정입니다');
     });
 }); 
